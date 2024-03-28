3b6e85f31679bdedc3c0ab1afc447f8d4e1c8a02
quydoan0316 <quydoan0316@gmail.com>
2024-03-28 10:40:07 +0700

    Implement another student services

--- src/main/java/com/hcmutap/elearning/service/IStudentService.java
+++ src/main/java/com/hcmutap/elearning/service/IStudentService.java
@@ -1,13 +1,24 @@
 package com.hcmutap.elearning.service;
 
+import com.hcmutap.elearning.model.ClassModel;
+import com.hcmutap.elearning.model.CourseModel;
 import com.hcmutap.elearning.model.StudentModel;
+import com.hcmutap.elearning.model.PointModel;
 
 import java.util.List;
 
 public interface IStudentService {
 	List<StudentModel> findAll();
-	void save(StudentModel studentModel);
+	String save(StudentModel studentModel);
 	void update(StudentModel studentModel);
 	void delete(String id);
+	StudentModel findById(String id); // Student information by studentID
+	void DangkiMonhoc(StudentModel studentModel, String courseID);
+	List<ClassModel> get_timetable(String studentId);
+	List<CourseModel> Tientrinhhoctap(String studentId);
+	List<PointModel> get_point(String studentId);
+	
+//	String getScore(StudentModel studentModel);
+//	void search_AccumulatedCredits(StudentModel studentModel);
 
 }
