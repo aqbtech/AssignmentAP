10fa228c8450115b737c86a599264ca79923ccb5
Tu Van Nguyen Anh Quan <127805529+aqbtech@users.noreply.github.com>
2024-03-29 21:37:19 +0700

    resolve conflict from Qyqy

--- src/main/java/com/hcmutap/elearning/service/impl/StudentService.java
+++ src/main/java/com/hcmutap/elearning/service/impl/StudentService.java
@@ -1,16 +1,15 @@
 package com.hcmutap.elearning.service.impl;
 
-import com.hcmutap.elearning.dao.CourseDAO;
 import com.hcmutap.elearning.dao.StudentDAO;
-import com.hcmutap.elearning.dao.PointDAO;
-import com.hcmutap.elearning.model.CourseModel;
-import com.hcmutap.elearning.model.PointModel;
 import com.hcmutap.elearning.model.StudentModel;
-import com.hcmutap.elearning.model.ClassModel;
+import com.hcmutap.elearning.repository.StudentRepository;
+import com.hcmutap.elearning.repository.document.StudentDocument;
 import com.hcmutap.elearning.service.IStudentService;
 import jakarta.annotation.Resource;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -19,10 +18,6 @@ public class StudentService implements IStudentService {
 //	private StudentRepository studentRepository;
 	@Resource
 	private StudentDAO studentDAO;
-	@Resource
-	private CourseDAO courseDAO;
-	@Resource
-	private  PointDAO pointDAO;
 	@Override
 	public List<StudentModel> findAll() {
 		return studentDAO.findAll();
@@ -42,38 +37,6 @@ public class StudentService implements IStudentService {
 	@Override
 	public StudentModel findById(String id){
 		return studentDAO.findById(id);
-	}
-	@Override
-	public void DangkiMonhoc(StudentModel studentModel, String courseID) {
-		CourseModel course = courseDAO.findById(courseID);
-		studentModel.getCourses().add(course);
-	}
-	@Override
-	public List<ClassModel> get_timetable(String studentId) {
-		StudentModel studentModel = studentDAO.findById(studentId);
-		return studentModel.getClasses();
-	}
-	@Override
-	public List<CourseModel> Tientrinhhoctap(String studentId){
-		StudentModel studentModel = studentDAO.findById(studentId);
-		List<ClassModel> classes = studentModel.getClasses();
-        for (ClassModel e : classes) {
-            if (e.getTimeEnd().equals(" ")) {
-//				if(point of this course is greater than or equal to 4)
-//				update finished_course by adding this course to list<CourseModel> finished_courses
-//				if(this course exist in  finished_courses and course's point greater than) then update
-				int x = 0;
-            }
-        }
-		List<CourseModel> finished_courses = studentModel.getFinished_courses();
-		return finished_courses;
-	}
-	@Override
-	public List<PointModel> get_point(String studentId){
-		StudentModel studentModel = studentDAO.findById(studentId);
-		List<PointModel> point = null;
-		point = pointDAO.findPoint(studentId);
-		return point;
 	}
 //	@Override
 //	public String getScore(StudentModel studentModel) {
