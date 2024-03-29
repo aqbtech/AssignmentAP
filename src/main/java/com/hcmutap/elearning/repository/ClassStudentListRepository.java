805c9e48a3b391beb15e1f2575d777dd49e684ad
M.Quân <quanvtt2004@gmail.com>
2024-03-29 10:35:03 +0700

    course_feature

--- src/main/java/com/hcmutap/elearning/repository/ClassStudentListRepository.java
+++
@@ -1,9 +0,0 @@
-package com.hcmutap.elearning.repository;
-
-import com.github.fabiomaffioletti.firebase.repository.DefaultFirebaseRealtimeDatabaseRepository;
-import com.hcmutap.elearning.repository.document.ClassStudentListDocument;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public class ClassStudentListRepository extends DefaultFirebaseRealtimeDatabaseRepository<ClassStudentListDocument, String> {
-}
