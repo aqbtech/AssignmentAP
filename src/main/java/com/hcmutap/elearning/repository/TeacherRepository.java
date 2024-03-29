d60d903bcd803a18ac84ea6c878060dea0ed70d9
M.Quân <quanvtt2004@gmail.com>
2024-03-29 20:29:36 +0700

    delete repository

--- src/main/java/com/hcmutap/elearning/repository/TeacherRepository.java
+++
@@ -1,9 +0,0 @@
-package com.hcmutap.elearning.repository;
-
-import com.github.fabiomaffioletti.firebase.repository.DefaultFirebaseRealtimeDatabaseRepository;
-import com.hcmutap.elearning.repository.document.TeacherDocument;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public class TeacherRepository extends DefaultFirebaseRealtimeDatabaseRepository<TeacherDocument, String> {
-}
