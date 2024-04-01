7f359cb167ed79b714e270a50835c3288e6205e2
M.Quân <quanvtt2004@gmail.com>
2024-04-01 14:43:34 +0700

    update adminCourseController

--- src/main/java/com/hcmutap/elearning/controller/admin/AdminCourseController.java
+++ src/main/java/com/hcmutap/elearning/controller/admin/AdminCourseController.java
@@ -1,22 +1,22 @@
 package com.hcmutap.elearning.controller.admin;
 
-import com.hcmutap.elearning.dto.CourseDTO;
+import com.hcmutap.elearning.controller.api.ClassAPI;
+import com.hcmutap.elearning.controller.api.CourseAPI;
+import com.hcmutap.elearning.model.ClassModel;
 import com.hcmutap.elearning.model.CourseModel;
-import com.hcmutap.elearning.service.ICourseService;
-import com.hcmutap.elearning.utils.MapperUtil;
 import jakarta.annotation.Resource;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class AdminCourseController {
 	@Resource
-	private ICourseService courseService;
+	private CourseAPI courseAPI;
+	@Resource
+	private ClassAPI classAPI;
 	// TODO: viết hàm addCourse để thêm một khóa học mới
 	// tham khảo hàm addAccount trong HomeController.java(GetMapping và PostMapping)
 	// yêu cầu nhận vào 1 html form (tên form là createCourse đã có trong resourses/templates/admin/views/createCourse.html)
@@ -35,11 +35,29 @@ public class AdminCourseController {
 		courseModel.setPercentBTL(Double.parseDouble(request.getParameter("percentBTL")));
 		courseModel.setPercentGK(Double.parseDouble(request.getParameter("percentGK")));
 		courseModel.setPercentCK(Double.parseDouble(request.getParameter("percentCK")));
-		courseService.save(courseModel);
-		return "redirect:/admin-management?type=" + "course";
+		courseAPI.save(courseModel);
+		return "redirect:/admin-management/add-course";
 	}
 
 
 	// TODO: addClasses để thêm một lớp học mới
+	@GetMapping("/admin-management/add-class")
+	public String addClass(ModelMap model) {
+		model.addAttribute("courses", courseAPI.findAll());
+		return "admin/views/createClass";
+	}
 
+	@PostMapping("/admin-management/add-class")
+	public String addClass(HttpServletRequest request) {
+		ClassModel classModel = new ClassModel();
+		classModel.setCourseId((request.getParameter("courseId")));
+		classModel.setClassName(request.getParameter("className"));
+		classModel.setClassId(classModel.getCourseId()+"-"+classModel.getClassName());
+		classModel.setDayOfWeek(request.getParameter("dayOfWeek"));
+		classModel.setTimeStart(request.getParameter("timeStart"));
+		classModel.setTimeEnd(request.getParameter("timeEnd"));
+		classModel.setRoom(request.getParameter("room"));
+		classAPI.save(classModel);
+		return "redirect:/admin-management/add-class";
+	}
 }
--- src/main/java/com/hcmutap/elearning/service/IClassStudentListService.java
+++
@@ -1,3 +0,0 @@
-package com.hcmutap.elearning.service;
-
-public interface IClassStudentListService {
