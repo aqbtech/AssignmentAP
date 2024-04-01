3ee88968c297c4d8762d4f22ce17dfa5bd6d3175
M.Quân <quanvtt2004@gmail.com>
2024-04-01 07:45:28 +0700

    create course

--- src/main/java/com/hcmutap/elearning/controller/admin/AdminCourseController.java
+++ src/main/java/com/hcmutap/elearning/controller/admin/AdminCourseController.java
@@ -1,7 +1,11 @@
 package com.hcmutap.elearning.controller.admin;
 
 import com.hcmutap.elearning.dto.CourseDTO;
+import com.hcmutap.elearning.model.CourseModel;
+import com.hcmutap.elearning.service.ICourseService;
 import com.hcmutap.elearning.utils.MapperUtil;
+import jakarta.annotation.Resource;
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,20 +15,28 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class AdminCourseController {
+	@Resource
+	private ICourseService courseService;
 	// TODO: viết hàm addCourse để thêm một khóa học mới
 	// tham khảo hàm addAccount trong HomeController.java(GetMapping và PostMapping)
 	// yêu cầu nhận vào 1 html form (tên form là createCourse đã có trong resourses/templates/admin/views/createCourse.html)
 	// va sau do goi service de them vao database
-	@GetMapping("/admin-management/addCourse")
-	public String addCourse(@RequestParam("type ") String type) {
-
-
+	@GetMapping("/admin-management/add-course")
+	public String addCourse() {
 		return "admin/views/createCourse";
 	}
-	@PostMapping("/admin-management/addCourse")
-	public String addCourse(@ModelAttribute CourseDTO courseDTO) {
-		ModelMap modelMap = MapperUtil.getInstance().toModelMapFromDTO(courseDTO);
-		return "redirect:/admin-management?type=" + courseDTO.getRole().toLowerCase();
+	@PostMapping("/admin-management/add-course")
+	public String addCourse(HttpServletRequest request) {
+		CourseModel courseModel = new CourseModel();
+		courseModel.setCourseId(request.getParameter("courseId"));
+		courseModel.setCourseName(request.getParameter("courseName"));
+		courseModel.setCredit(Integer.parseInt(request.getParameter("credit")));
+		courseModel.setPercentBT(Double.parseDouble(request.getParameter("percentBT")));
+		courseModel.setPercentBTL(Double.parseDouble(request.getParameter("percentBTL")));
+		courseModel.setPercentGK(Double.parseDouble(request.getParameter("percentGK")));
+		courseModel.setPercentCK(Double.parseDouble(request.getParameter("percentCK")));
+		courseService.save(courseModel);
+		return "redirect:/admin-management?type=" + "course";
 	}
 
 
