72483247fbd08329261ca17af86fdcb7d82a3676
quydoan0316 <quydoan0316@gmail.com>
2024-04-05 20:35:35 +0700

    Student Service view

--- src/main/java/com/hcmutap/elearning/controller/web/StudentController.java
+++ src/main/java/com/hcmutap/elearning/controller/web/StudentController.java
@@ -1,4 +1,49 @@
 package com.hcmutap.elearning.controller.web;
 
-public class StudentController {
+
+import com.hcmutap.elearning.dto.InfoDTO;
+import com.hcmutap.elearning.model.ClassModel;
+import com.hcmutap.elearning.model.CourseModel;
+import com.hcmutap.elearning.model.StudentModel;
+import com.hcmutap.elearning.service.IStudentService;
+import com.hcmutap.elearning.service.ITeacherService;
+import com.hcmutap.elearning.service.impl.CourseFacade;
+import com.hcmutap.elearning.service.impl.UserService;
+import jakarta.annotation.Resource;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.security.Principal;
+import java.util.List;
+
+@Controller
+@RequestMapping(value = "/student")
+public class StudentController{
+    @Resource
+    private UserService userService;
+    @Resource
+    private IStudentService studentService;
+
+    @RequestMapping("/service")
+    public String service(){return "web/views/student";}
+
+    @GetMapping(value = "/registration")
+    public String regis(Principal principal,ModelMap model){
+        InfoDTO infoDTO = userService.getInfo(principal.getName());
+        StudentModel studentModel = studentService.findById(infoDTO.getId());
+        List<CourseModel> courses = studentService.get_course(studentModel.getStudentId());
+        model.addAttribute("courses", courses);
+        return "web/views/student-service/registration";
+    }
+    @GetMapping(value = "/timetable")
+    public String timetable(Principal principal,ModelMap model){
+        InfoDTO infoDTO = userService.getInfo(principal.getName());
+        StudentModel studentModel = studentService.findById(infoDTO.getId());
+        List<ClassModel> classes = studentService.get_timetable(studentModel.getStudentId());
+        model.addAttribute("classes", classes);
+        return "web/views/student-service/time-table";
+    }
+
 }
