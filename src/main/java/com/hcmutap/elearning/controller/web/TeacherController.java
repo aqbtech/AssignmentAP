b628f054f7b121d5f2bc4f9c9268418a35189ed3
toandemoo <toanjhino1@gmail.com>
2024-04-17 17:10:19 +0700

    udatesacorggg

--- src/main/java/com/hcmutap/elearning/controller/web/TeacherController.java
+++ src/main/java/com/hcmutap/elearning/controller/web/TeacherController.java
@@ -1,10 +1,12 @@
 package com.hcmutap.elearning.controller.web;
 
+import com.hcmutap.elearning.controller.admin.HomeController;
 import com.hcmutap.elearning.dto.Class_CourseDTO;
 import com.hcmutap.elearning.dto.InfoDTO;
 import com.hcmutap.elearning.dto.PointDTO;
 import com.hcmutap.elearning.exception.NotFoundException;
 import com.hcmutap.elearning.model.ClassModel;
+import com.hcmutap.elearning.model.CourseModel;
 import com.hcmutap.elearning.model.PointModel;
 import com.hcmutap.elearning.model.TeacherModel;
 import com.hcmutap.elearning.service.*;
@@ -12,6 +14,8 @@ import com.hcmutap.elearning.service.impl.Class_CourseService;
 import com.hcmutap.elearning.service.impl.CourseService;
 import com.hcmutap.elearning.service.impl.UserService;
 import jakarta.annotation.Resource;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.*;
@@ -24,7 +28,7 @@ import java.util.List;
 @Controller
 @RequestMapping(value = "/teacher")
 public class TeacherController {
-
+	private static final Logger logger = LoggerFactory.getLogger(TeacherController.class);
 	@Resource
 	private UserService userService;
 	@Resource
@@ -39,8 +43,6 @@ public class TeacherController {
 	private Class_CourseService class_courseService;
 	@Resource
 	private CourseService courseService;
-	@Resource
-	private ISemesterService semesterServicel;
 
 	@GetMapping(value = "/course")
 	public String teacher(@RequestParam("id") int id) {
@@ -55,10 +57,6 @@ public class TeacherController {
 
 			List<Class_CourseDTO> class_course = new ArrayList<>();
 			List<Class_CourseDTO> class_course_of_teacher = class_courseService.getClass_Course(teacherModel.getUsername());
-
-			if(!class_courseService.checkClass_Course(class_course_of_teacher)){
-				return "login/Rare_fault";
-			}
 
 			if(id.isEmpty()){
 				model.addAttribute("class_course_of_teacher", class_course_of_teacher);
@@ -130,69 +128,54 @@ public class TeacherController {
 		}
 	}
 	@GetMapping(value = "/update_student")
-	public String listStudent(@RequestParam("classId") String classId,@RequestParam("courseId") String courseId, Principal principal, ModelMap model){
+	public String listStudent(@RequestParam("classId") String classId,
+							  @RequestParam("courseId") String courseId,
+							  final RedirectAttributes redirectAttributes,
+							  Principal principal, ModelMap model){
 		try {
 			InfoDTO infoDTO = userService.getInfo(principal.getName());
 			TeacherModel teacherModel = teacherService.findById(infoDTO.getId());
-
+			CourseModel courseModel = courseService.getCourseInfo(courseId);
 			List<PointModel> pointModels = pointService.getListStudentByClassId(classId);
-			model.addAttribute("classId",classId);
-			model.addAttribute("courseId",courseId);
 			model.addAttribute("points", pointModels);
 			model.addAttribute("name","Danh sách lớp "+pointModels.getFirst().getClassName() +" - khóa học " +pointModels.getFirst().getCourseName());
+			model.addAttribute("classId",classId);
+			model.addAttribute("courseId",courseId);
+			model.addAttribute("course",courseModel);
+			return "web/views/teacher-service/InputScore/list_student";
+		} catch (NotFoundException e) {
+			logger.error("Error in update_student");
+			return "redirect:/teacher/update_point";
+		}
+	}
+	@PostMapping(value = "/update_student")
+	public String listStudentt(@RequestParam("studentId") String studentId,
+                               @RequestParam("courseId") String courseId,
+                               @RequestParam("classId") String classId,
+							   @ModelAttribute PointDTO pointDTO,
+                               Principal principal, ModelMap model){
+		try {
+			InfoDTO infoDTO = userService.getInfo(principal.getName());
+			TeacherModel teacherModel = teacherService.findById(infoDTO.getId());
+			CourseModel courseModel = courseService.getCourseInfo(courseId);
+			classService.NhapDiem(studentId,classId,pointDTO);
 
+			List<PointModel> pointModels = null;
+			pointModels = pointService.getListStudentByClassId(classId);
+			model.addAttribute("points", pointModels);
+			model.addAttribute("name","Danh sách lớp "+pointModels.getFirst().getClassName() +" - khóa học " +pointModels.getFirst().getCourseName());
+			model.addAttribute("classId",classId);
+			model.addAttribute("courseId",courseId);
+			model.addAttribute("course",courseModel);
 			return "web/views/teacher-service/InputScore/list_student";
 		} catch (NotFoundException e) {
 			throw new RuntimeException(e);
-		}
+        }
 	}
 
 	@GetMapping(value = "/update_point")
-	public String updatePoint(@RequestParam("courseId") String courseId,@RequestParam("studentId") String studentId,Principal principal, ModelMap model){
-		try {
-			InfoDTO infoDTO = userService.getInfo(principal.getName());
-			TeacherModel teacherModel = teacherService.findById(infoDTO.getId());
-
-			PointModel pointModel = pointService.getPoint(studentId,courseId);
-
-			model.addAttribute("studentId",studentId);
-			model.addAttribute("courseId",courseId);
-			model.addAttribute("point",pointModel);
-			model.addAttribute("name","Sinh viên "+pointModel.getStudentName()+" - lớp " +pointModel.getClassName() +" - khóa học " +pointModel.getCourseName());
-
+	public String listStudent(ModelMap modelMap){
+			modelMap.addAttribute("name","Hiện không thể tìm thấy sinh viên ");
 			return "web/views/teacher-service/InputScore/UpdatePoint";
-		} catch (NotFoundException e) {
-			throw new RuntimeException(e);
-		}
-	}
-
-	@PostMapping(value = "/update_point")
-	public String UpdatePoint(@RequestParam("courseId") String courseId,
-							  @RequestParam("studentId") String studentId,
-							  @ModelAttribute PointDTO pointDTO,
-							  Principal principal,
-							  ModelMap model){
-		try {
-			InfoDTO infoDTO = userService.getInfo(principal.getName());
-			TeacherModel teacherModel = teacherService.findById(infoDTO.getId());
-
-			PointModel pointModel = pointService.getPoint(studentId,courseId);
-//			PointModel pointModel = pointService.getPoint("2213001","XSTK");
-			pointModel.setPointBT(pointDTO.getPointBT());
-			pointModel.setPointBTL(pointDTO.getPointBTL());
-			pointModel.setPointGK(pointDTO.getPointGK());
-			pointModel.setPointCK(pointDTO.getPointCK());
-			pointService.update(pointModel);
-
-			model.addAttribute("studentId",studentId);
-			model.addAttribute("courseId",courseId);
-			model.addAttribute("classId",pointModel.getClassId());
-			model.addAttribute("point",pointModel);
-			model.addAttribute("name","Sinh viên "+pointModel.getStudentName()+" - lớp " +pointModel.getClassName() +" - khóa học " +pointModel.getCourseName());
-
-			return "web/views/teacher-service/InputScore/UpdatePoint";
-		} catch (NotFoundException e) {
-			throw new RuntimeException(e);
-		}
 	}
 }
